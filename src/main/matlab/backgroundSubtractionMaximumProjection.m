ccb8a1bd80b416f9fa1b550bb5e343133c628667
haesleinhuepf <rhaase@mpi-cbg.de>
2019-08-28 22:19:57 +0200

    add more matlab example scripts

---
+++ src/main/matlab/backgroundSubtractionMaximumProjection.m
@@ -0,0 +1,65 @@
+% backgroundSubtractioMaximumProjection.m
+%
+% This script shows how to run CLATLAB for GPU accelerated image processing
+% to do background subtraction and maximum projection in MATLAB.
+%
+% In order to make this script run, you need to install CLATLAB
+%         https://clij.github.io/clatlab/
+% 
+% Author: Robert Haase, rhaase@mpi-cbg.de
+%         August 2019
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% initialize CLATLAB
+clatlab = init_clatlab();
+clop = clatlab.op;
+
+% check on which GPU it's running 
+string(clatlab.getGPUName())
+
+% load example data
+filename = '../../test/resources/Nantes_000646.tif';
+info = imfinfo(filename);
+numberOfImages = numel(info);
+image = [];
+for i = 1:numberOfImages
+    plane = imread(filename,i);
+    if i == 1
+        image = plane;
+    else 
+        image = cat(3, image, plane);
+    end
+end 
+% image = permute(image, [1 2 3]);
+
+image = double(image);
+
+% push image to GPU memory
+input = clatlab.push(image);
+originalSize = op().getSize(input);
+
+
+% maximum projection
+maximumProjected = clatlab.create(originalSize(1:2));
+clop.maximumZProjection(input, maximumProjected);
+figure;
+subplot(1,2,1), imshow(clatlab.pull(maximumProjected), [0 1000]);
+
+% background subtraction
+backgroundSubtracted = clatlab.create(input);
+import java.lang.Float;
+sigmaXY = Float(5);
+sigmaZ = Float(1);
+clop.subtractBackground(input, backgroundSubtracted, sigmaXY, sigmaXY, sigmaZ);
+
+% maximum projection
+clop.maximumZProjection(backgroundSubtracted, maximumProjected);
+
+% pull result back from GPU and show it
+subplot(1,2,2), imshow(clatlab.pull(maximumProjected), [0 200]);
+
+% cleanup
+maximumProjected.close();
+backgroundSubtracted.close();
+input.close();
---
+++ src/main/matlab/segmentation.m
@@ -0,0 +1,59 @@
+% thresholding.m
+%
+% This script shows how to run CLATLAB for GPU accelerated image processing
+% from MATLAB. It applies blurring and tresholding to an image.
+%
+% Author: Robert Haase, rhaase@mpi-cbg.de
+%         August 2019
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% initialize CLATLAB
+clatlab = init_clatlab();
+op = clatlab.op;
+
+% load example data
+filename = '../../test/resources/blobs.tif';
+img = imread(filename);
+% there are issues with unit8/int8 conversion; 
+% thus, we convert the image to double
+img = double(img);
+
+% show input image in a subplot
+figure;
+subplot(1,2,1), imshow(img, [0 255]);
+
+% import and initialize CLATLAB
+clatlab = net.haesleinhuepf.clatlab.CLATLAB.getInstance();
+
+% check on which GPU it's running 
+string(clatlab.getGPUName())
+
+% push image to GPU memory
+input = clatlab.push(img);
+% reserve memory for output image
+blurred = clatlab.create(input);
+thresholded = clatlab.create(input);
+labelled = clatlab.create(input);
+
+% blur the image
+import java.lang.Float;
+clop.blur(input, blurred, Float(5), Float(5));
+
+% apply a threshold to it
+clop.automaticThreshold(blurred, thresholded, "Otsu");
+
+% connected components labelling
+clop.connectedComponentsLabeling(thresholded, labelled);
+
+% pull result back from GPU and show it next to input
+result = clatlab.pull(labelled);
+lookuptable = rand(op.maximumOfAllPixels(labelled), 3);
+subplot(1,2,2), imshow(result, lookuptable);
+
+% clean up
+input.close();
+blurred.close();
+thresholded.close();
+
