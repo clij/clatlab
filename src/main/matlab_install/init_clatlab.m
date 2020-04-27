0172f026ffb821fe1cb185e30f65a3d171907477
haesleinhuepf <rhaase@mpi-cbg.de>
2020-04-27 12:35:20 +0200

    added separate folder for matlab/clij2 internals

---
+++ src/main/matlab_install/init_clatlab.m
@@ -0,0 +1,22 @@
+% init_clatlab.m
+%
+% This is a utiliy function simplifying CLATLAB initialisation.
+% You may have to change clatlab_folder variable below to fit your needs.
+%
+%
+% In order to make this script run, you need to install CLATLAB an
+% run it from matlab. Tested with Matlab 2019b
+%         https://clij.github.io/clatlab/
+% 
+% Author: Robert Haase, rhaase@mpi-cbg.de
+%         August 2019
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function clatlab_ = init_clatlab()
+    if (exist('net.haesleinhuepf.clatlab.CLATLAB') ~= 8)
+		jar_location = strrep(mfilename('fullpath'), 'init_clatlab', 'clatlab-0.27.0-jar-with-dependencies.jar');
+        javaaddpath(jar_location);        
+    end
+    clatlab_ = net.haesleinhuepf.clatlab.CLATLAB.getInstance();
+end
